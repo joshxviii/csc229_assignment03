6253b98a3f93c5fac44282d3e7a4f1affd80b041
Josh <125091065+joshxviii@users.noreply.github.com>
2024-02-11 16:41:50 -0500

    comments and gitignore

--- src/main/java/assignment_package/Algorithm.java
+++ src/main/java/assignment_package/Algorithm.java
@@ -1,24 +1,21 @@
 package assignment_package;
-/**
- * @author Josh
- */
+
 public class Algorithm {
-
-    public static int getLargest(int arr[], int sz) {
-        int iterate1 = 0;
-        int iterate2 = 0;
-        int largest = 0;
-        while (iterate1 < sz - 1) {
-            iterate2++;
-            if (iterate2 == sz) {
-                iterate1++;
-                iterate2 = iterate1;
-                continue;
-            }
-            int product = arr[iterate1] = arr[iterate2];
-            if (product > largest) largest = product;
-        }
-        return largest;
-    }
-
+    public static int getLargest(int arr[], int sz) {       // N = sz
+                                                            // *times ran
+        int iterate1 = 0;                                   // 1
+        int iterate2 = 0;                                   // 1
+        int largest = 0;                                    // 1
+        while (iterate1 < sz - 1) {                         // N
+            iterate2++;                                     // N
+            if (iterate2 == sz) {                           // N
+                iterate1++;                                 // 1
+                iterate2 = iterate1;                        // 1
+                continue;                                   // 1
+            }                                               //
+            int product = arr[iterate1] = arr[iterate2];    // N-1
+            if (product > largest) largest = product;       // N-1
+        }                                                   //
+        return largest;                                     // 1
+    }                                                       // O(N)
 }
--- src/main/java/assignment_package/Driver.java
+++ src/main/java/assignment_package/Driver.java
@@ -6,6 +6,12 @@ public class Driver {
 
     public static void main(String[] args) {
 
+        Algorithm algorithm = new Algorithm();
+
+        int[] list = {1, 2, 32, 90, 1, 32, 40, 120, 53, 12};
+
+        System.out.println( algorithm.getLargest(list, list.length) );
+
     }
 
 }
